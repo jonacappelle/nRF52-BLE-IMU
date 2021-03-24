3824ec0eb29f850ab7febe6e18e150dadb6f8932
jonacappelle <jonacappelle@hotmail.com>
2021-03-24 15:21:15 +0100

    BLE code cleanup

--- pca10040/s132/arm5_no_packs/usr_ble.h
+++ pca10040/s132/arm5_no_packs/usr_ble.h
@@ -1,8 +1,12 @@
 #ifndef _USR_BLE_H_
 #define _USR_BLE_H_
 
+#include <stdbool.h>
+#include <stdint.h>
 
-
+uint32_t usr_ble_init(void);
+uint32_t nus_printf_custom(char* p_char);
+void idle_state_handle(void);
 
 
 #endif
